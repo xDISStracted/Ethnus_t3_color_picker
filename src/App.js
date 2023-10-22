8793165fadc37695ccc7dc742edc8abcb7b64f6f
Prakhar Vishwas <144513343+xDISStracted@users.noreply.github.com>
2023-10-22 16:40:47 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from './components/Container';
+
+function App() {
+  return (
+    <Container/>
+  );
+}
+
+export default App;
